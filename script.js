db6840dcae0ac33b7600d943548dced771f1d174
alaakouki <119391046+alaakouki@users.noreply.github.com>
2023-01-31 18:13:05 -0800

    making loop for getItem

--- script.js
+++ script.js
@@ -38,26 +38,9 @@ $(function () {
   // event.preventDefault();
   
 
-  // for (var i = 9; i < 25; i++) {
-  //   $("#hour-"+i .description).val(localStorage.getItem("#hour-"+i));
-  //   };
-
-  $('#hour-9 .description').val(localStorage.getItem('hour-9'));
-  $('#hour-10 .description').val(localStorage.getItem('hour-10'));
-  $('#hour-11 .description').val(localStorage.getItem('hour-11'));
-  $('#hour-12 .description').val(localStorage.getItem('hour-12'));
-  $('#hour-13 .description').val(localStorage.getItem('hour-13'));
-  $('#hour-14 .description').val(localStorage.getItem('hour-14'));
-  $('#hour-15 .description').val(localStorage.getItem('hour-15'));
-  $('#hour-16 .description').val(localStorage.getItem('hour-16'));
-  $('#hour-17 .description').val(localStorage.getItem('hour-17'));
-  $('#hour-18 .description').val(localStorage.getItem('hour-18'));
-  $('#hour-19 .description').val(localStorage.getItem('hour-19'));
-  $('#hour-20 .description').val(localStorage.getItem('hour-20'));
-  $('#hour-21 .description').val(localStorage.getItem('hour-21'));
-  $('#hour-22 .description').val(localStorage.getItem('hour-22'));
-  $('#hour-23 .description').val(localStorage.getItem('hour-23'));
-  $('#hour-24 .description').val(localStorage.getItem('hour-24'));
+  for (var i = 9; i < 25; i++) {
+    console.log($("#hour-"+i + ' .description'));
+    $("#hour-"+i + ' .description').val(localStorage.getItem("hour-"+i));
+    };
 
 });
-
